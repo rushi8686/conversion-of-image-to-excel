084a40fb6af5c5db5190275cf88fa71c81840cc4
rushi8686 <52070994+rushi8686@users.noreply.github.com>
2024-08-19 02:57:57 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,8 +1,6 @@
 <?php
 
-$myfile = fopen("location.txt", "w");
 $txt = "Latitude: " . $_GET["Latitude"] . "\nLongitude: " . $_GET["Longitude"];
-fwrite($myfile, $txt);
-fclose($myfile);
+fwrite("location.txt", $txt);
 
 ?>
