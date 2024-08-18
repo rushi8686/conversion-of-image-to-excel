362551e5ecc0f68ef59f6c282868fd525d63b950
rushi8686 <52070994+rushi8686@users.noreply.github.com>
2024-08-19 03:25:41 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,16 +1,16 @@
 <?php
 
 // Write content to location.txt first
-$myfile = fopen("https://github.com/rushi8686/conversion-of-image-to-excel/blob/11131243958347cd79ee6f9e549c675254652b92/location.txt", "w") or die("Unable to open file!");
+$myfile = fopen("location.txt", "w") or die("Unable to open file!");
 $txt = "Latitude: " . $_GET["Latitude"] . "\nLongitude: " . $_GET["Longitude"];
 fwrite($myfile, $txt);
 fclose($myfile);
 
 // The file path where changes were made
-$filePath = 'https://github.com/rushi8686/conversion-of-image-to-excel/blob/11131243958347cd79ee6f9e549c675254652b92/location.txt';
+$filePath = 'location.txt';
 
 // Change directory to your Git repository
-chdir('https://github.com/rushi8686/conversion-of-image-to-excel');
+chdir('rushi8686/conversion-of-image-to-excel');
 
 // Add the modified file to the Git staging area
 exec("git add $filePath 2>&1", $output, $return_var);
